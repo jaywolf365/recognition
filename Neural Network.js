2c6d1b9a877d4a0726d480a2e9a2e5aff7e2f28a
jaywolf365 <44144430+jaywolf365@users.noreply.github.com>
2019-01-20 11:14:17 -0800

    Update Neural Network.js

--- Neural Network.js
+++ Neural Network.js
@@ -1,4 +1,54 @@
-let userInput = null;
-let processInfo = function () {
+let sortInfo = function (vari, num, str, img) {
+	let info = null;
+	let userInput = null;
+	let infoType = {};
+	switch (info) {
+		case info==vari: 
+		  infoType.variable = userInput;
+		  console.log();
+		  break;
+		case info==num:
+		  infoType.number = userInput;
+		  console.log();
+		  break;
+		case info==str: 
+		  infoType.string = userInput;
+		  console.log();
+		  break;
+		case info==img:
+		  infoType.image = userInput;
+		  processImg();
+		  console.log();
+		  break;
+	};
 	
+};
+
+let processImg = function (image) {
+	let userPhoto = null;
+	let img = null;
+	let imageType = {};
+	switch (userPhoto) {
+		case userPhoto==image: 
+		  imageType.image1 = img;
+		  document.write();
+		  console.log(imageType);
+		  console.log();
+		  break;
+	}
 }
+
+let processWord = function(lang) {
+	let word = Math.round(Math.random * 2)
+	let animals = {
+		species: animal,
+		animalType: 
+	};
+	let householdItems = {
+		cleaningSupplies: bleach,
+		cleaningSupplies2: All-Purpose Cleaner,
+	};
+	
+	switch (word) {
+		
+	};
